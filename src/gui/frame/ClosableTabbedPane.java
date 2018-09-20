bd0f20b75fd0052a0ebe7ba70a0c741c57af8f0d
liupf <569122151@qq.com>
2018-09-20 10:22:42 +0800

    page layout

---
+++ src/gui/frame/ClosableTabbedPane.java
@@ -0,0 +1,126 @@
+package gui.frame;
+
+
+import javax.swing.JFrame;  
+import javax.swing.JPanel;  
+import javax.swing.JTabbedPane;  
+import java.awt.Rectangle;  
+import java.awt.Color;  
+import java.awt.event.MouseListener;  
+import java.awt.Component;  
+import javax.swing.Icon;  
+import java.awt.event.MouseEvent;  
+import java.awt.Graphics;  
+
+/** 
+*自定义带关闭按钮的TabbedPane 
+* @author  XiaoQuan 
+*/  
+public class ClosableTabbedPane extends JTabbedPane implements MouseListener {  
+
+  private static final long serialVersionUID = -5154435928212725041L;  
+
+  public ClosableTabbedPane() {  
+      super();  
+      addMouseListener(this);  
+  }  
+
+  @Override  
+  public void addTab(String title, Component component) {  
+      super.addTab(title, null, component);  
+  }  
+
+  public void addTab(String title, Component component, Icon extraIcon) {  
+      super.addTab(title, new CloseTabIcon(), component);  
+  }  
+
+  @Override  
+  public void mouseClicked(MouseEvent e) {  
+      int tabNumber = getUI().tabForCoordinate(this, e.getX(), e.getY());  
+      if (tabNumber < 0) {  
+          return;  
+      }  
+      if (((CloseTabIcon) getIconAt(tabNumber)) != null) {  
+          Rectangle rect = ((CloseTabIcon) getIconAt(tabNumber)).getBounds();  
+          if (rect.contains(e.getX(), e.getY())) {  
+              this.removeTabAt(tabNumber);  
+          }  
+      }  
+  }  
+
+  public void mouseEntered(MouseEvent e) {  
+  }  
+
+  public void mouseExited(MouseEvent e) {  
+  }  
+
+  public void mousePressed(MouseEvent e) {  
+  }  
+
+  public void mouseReleased(MouseEvent e) {  
+  }  
+    
+  public static void main(String args[]){  
+      JFrame f =new JFrame();  
+        
+      ClosableTabbedPane closableTabbedPane =new ClosableTabbedPane();  
+      JPanel p1 = new JPanel();  
+      JPanel p2 = new JPanel();  
+        
+      closableTabbedPane.addTab("不带关闭按钮", p1);  
+        
+      closableTabbedPane.addTab("带关闭按钮", p2, null);  
+        
+      f.getContentPane().add(closableTabbedPane);  
+      f.setSize(500,400);  
+      f.setVisible(true);  
+      f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+  }  
+}  
+
+class CloseTabIcon implements Icon {  
+
+  private int x_pos;  
+  private int y_pos;  
+  private int width;  
+  private int height;  
+
+  public CloseTabIcon() {  
+      width = 16;  
+      height = 16;  
+  }  
+
+  @Override  
+  public void paintIcon(Component c, Graphics g, int x, int y) {  
+      this.x_pos = x;  
+      this.y_pos = y;  
+      Color col = g.getColor();  
+      g.setColor(Color.RED.darker());  
+      int y_p = y + 2;  
+      g.drawLine(x + 1, y_p, x + 12, y_p);  
+      g.drawLine(x + 1, y_p + 13, x + 12, y_p + 13);  
+      g.drawLine(x, y_p + 1, x, y_p + 12);  
+      g.drawLine(x + 13, y_p + 1, x + 13, y_p + 12);  
+      g.drawLine(x + 3, y_p + 3, x + 10, y_p + 10);  
+      g.drawLine(x + 3, y_p + 4, x + 9, y_p + 10);  
+      g.drawLine(x + 4, y_p + 3, x + 10, y_p + 9);  
+      g.drawLine(x + 10, y_p + 3, x + 3, y_p + 10);  
+      g.drawLine(x + 10, y_p + 4, x + 4, y_p + 10);  
+      g.drawLine(x + 9, y_p + 3, x + 3, y_p + 9);  
+      g.setColor(col);  
+  }  
+
+  @Override  
+  public int getIconWidth() {  
+      return width;  
+  }  
+
+  @Override  
+  public int getIconHeight() {  
+      return height;  
+  }  
+
+  public Rectangle getBounds() {  
+      return new Rectangle(x_pos, y_pos, width, height);  
+  }  
+} 
--- src/gui/frame/MainFrame.java
+++ src/gui/frame/MainFrame.java
@@ -1,14 +1,108 @@
 package gui.frame;
 
-/**
- * 主窗体
- * @author liupf
- *
- */
+import java.awt.EventQueue;
+import java.awt.Image;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.GroupLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JTabbedPane;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTable;
+import javax.swing.JSplitPane;
+import java.awt.FlowLayout;
+import javax.swing.JSpinner;
+import javax.swing.JSeparator;
+
 public class MainFrame extends BaseFrame {
-    public MainFrame() {
+
+    private static final long serialVersionUID = 9154006143553537232L;
+    private JFrame frame;
+
+    private ClosableTabbedPane tabbedPane;
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    MainFrame window = new MainFrame();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
-	private static final long serialVersionUID = 3354878488469592363L;
-	
-	
+
+    public MainFrame() {
+        initialize();
+    }
+
+    private void initialize() {
+        frame = new JFrame();
+        frame.setTitle("案件管理系统");
+        frame.setBounds(100, 100, 565, 397);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.getContentPane().setLayout(new BorderLayout(0, 0));
+        
+        tabbedPane = new ClosableTabbedPane();
+
+        frame.getContentPane().add(tabbedPane, BorderLayout.CENTER);
+        
+        JSplitPane splitPane = new JSplitPane();
+        tabbedPane.addTab("主页", null, splitPane, null);
+        JPanel panel = new JPanel();
+        splitPane.setLeftComponent(panel);
+        
+        JPanel panel_1 = new JPanel();
+        splitPane.setRightComponent(panel_1);
+        splitPane.setDividerLocation(0.5);
+        
+        JMenuBar menuBar = new JMenuBar();
+        frame.setJMenuBar(menuBar);
+
+        JMenu createMenu = new JMenu("新建");
+        menuBar.add(createMenu);
+        JMenu saveMenu = new JMenu("保存");
+        menuBar.add(saveMenu);
+
+        JMenuItem caseMenuItem = new JMenuItem("新建案件");
+        caseMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JPanel createPanel = new JPanel();
+                tabbedPane.addTab("新建案件", createPanel, null);
+            }
+        });
+        createMenu.add(caseMenuItem);
+
+        JMenuItem lawsMenuItem = new JMenuItem("新建法规");
+        lawsMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JPanel createPanel = new JPanel();
+                tabbedPane.addTab("新建法规", createPanel, null);
+            }
+        });
+        createMenu.add(lawsMenuItem);
+
+    }
+
+    public void addTable() {
+        Object[][] playerInfo = {
+                { "明天10:00上街" }, { "明天14:00交报告" }};
+        String[] Names = { "XX" };
+    }
 }
